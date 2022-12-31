1da504969ca0434c4d05c9993f176a157a96c4e4
Davi Castro Silva <davigamescastrosilva@gmail.com>
2022-12-30 22:36:34 -0300

    Primeira pergunta e leitura da mesma adicionada

---
+++ src/Desafio.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Desafio {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Digite um número: ");
+		int n1 = sc.nextInt();
+		
+		sc.close();
+
+	}
+
+}
